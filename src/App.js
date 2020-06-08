54f99622557fcf0e2dc71d3d017783eff17453b0
ndvssankar <sivashankar@msitprogram.net>
2020-06-08 19:48:12 +0530

    added login feature to master branch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GoogleBtn from './GoogleBtn';
+
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleBtn/>
+    </div>
+  );
+}
+
+export default App;
+
+
